88a370778e5dde329ec758959adb42936bd53e17
Kühlburger Tibor <kuhlburger@gmail.com>
2023-09-07 15:58:31 +0200

    2

---
+++ create_image_on_proxmox.sh
@@ -0,0 +1,26 @@
+#!/bin/bash  
+
+#https://austinsnerdythings.com/2021/08/30/how-to-create-a-proxmox-ubuntu-cloud-init-image/
+
+#1 – Install packages
+apt update -y && apt install libguestfs-tools -y
+
+
+#2. get and customize image
+wget=/usr/bin/wget
+wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
+
+
+
+
+
+#3 – Create a Proxmox virtual machine using the newly modified image
+sudo qm create 9000 --name "ubuntu-2004-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+sudo qm importdisk 9000 jammy-server-cloudimg-amd64.img local-lvm
+sudo qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
+sudo qm set 9000 --boot c --bootdisk scsi0
+sudo qm set 9000 --ide2 local-lvm:cloudinit
+sudo qm set 9000 --serial0 socket --vga serial0
+sudo qm set 9000 --agent enabled=1
+
