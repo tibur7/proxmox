7cb05298858a62b3c842c12ff496945f18496c95
Kühlburger Tibor <kuhlburger@gmail.com>
2023-09-07 16:30:43 +0200

    temaple vm parameters

--- template_to_vm.sh
+++ template_to_vm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo qm clone 9000 999 --name test-clone-cloud-init
+echo "Template: $1"
+echo "VM id: $2"
+echo "VM name: $3"
+echo "IP address: $4"
+echo "Gateway address: $5"
+
+sudo qm clone $1 $2 --name $3
 
 
-sudo qm set 999 --sshkey ~/.ssh/id_rsa.pub
-sudo qm set 999 --ipconfig0 ip=10.98.1.96/24,gw=10.98.1.1
+sudo qm set $2 --sshkey ~/.ssh/id_rsa.pub
+sudo qm set $2 --ipconfig0 ip=$4/24,gw=$5
 
-sudo qm start 999
+sudo qm start $2
